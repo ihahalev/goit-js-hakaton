8ac29ca17646e0bdba9ef1919722c9fbcc2f6e20
a-desna <a.sychuk@ukr.net>
2020-08-12 23:41:27 +0300

    “movie_page_pagination”

--- src/components/movieHomePage/index.js
+++ src/components/movieHomePage/index.js
@@ -1,4 +1,5 @@
 import movieAPI from '../../utils/movieAPI';
+import moviePagePagination from '../moviePagePagination';
 
 export default function (root) {
   const template = () => {
@@ -10,4 +11,6 @@ export default function (root) {
   root.insertAdjacentHTML('beforeend', `<div>movieHomePage</div>`);
   movieAPI.totalPages = 5;
   movieAPI.fetchMoviesPopularDay().then(res => console.log(res));
+
+  moviePagePagination(root);
 }
--- src/utils/movieAPI.js
+++ src/utils/movieAPI.js
@@ -4,44 +4,121 @@ axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 const KEY = '676b050b9e1ae3ff4b7bb472f439b690';
 const IMG_SERVER = 'https://image.tmdb.org/t/p/w500';
 
-const fetchMoviesPopularDay = (page = 1) => {
-  return axios
-    .get(`/trending/movie/day?page=${page}&api_key=${KEY}`)
-    .then(responce => {
-      return responce.data;
-    });
-};
+// const fetchMoviesPopularDay = (page = 1) => {
+//   return axios
+//     .get(`/trending/movie/day?page=${page}&api_key=${KEY}`)
+//     .then(responce => {
+//       return responce.data;
+//     });
+// };
 
-const fetchMoviesWithQuery = (query, page = 1) => {
-  return axios
-    .get(`/search/movie?query=${query}&page=${page}&api_key=${KEY}`)
-    .then(responce => {
-      return responce.data;
-    });
-};
+// const fetchMoviesWithQuery = (query, page = 1) => {
+//   return axios
+//     .get(`/search/movie?query=${query}&page=${page}&api_key=${KEY}`)
+//     .then(responce => {
+//       return responce.data;
+//     });
+// };
 
-const fetchMovieDetails = id => {
-  return axios.get(`/movie/${id}?api_key=${KEY}`).then(responce => {
-    return responce.data;
-  });
-};
+// const fetchMovieDetails = id => {
+//   return axios.get(`/movie/${id}?api_key=${KEY}`).then(responce => {
+//     return responce.data;
+//   });
+// };
 
-const fetchMovieCredits = id => {
-  return axios.get(`/movie/${id}/credits?api_key=${KEY}`).then(responce => {
-    return responce.data.cast;
-  });
-};
+// const fetchMovieCredits = id => {
+//   return axios.get(`/movie/${id}/credits?api_key=${KEY}`).then(responce => {
+//     return responce.data.cast;
+//   });
+// };
 
-const fetchMovieReviews = id => {
-  return axios.get(`/movie/${id}/reviews?api_key=${KEY}`).then(responce => {
-    return responce.data.results;
-  });
-};
+// const fetchMovieReviews = id => {
+//   return axios.get(`/movie/${id}/reviews?api_key=${KEY}`).then(responce => {
+//     return responce.data.results;
+//   });
+// };
+
+// export default {
+//   page: 1,
+//   pages: 2,
+//   query: '',
+//   get searchQuery() {
+//     return this.query;
+//   },
+//   set searchQuery(str) {
+//     this.query = encodeURIComponent(str);
+//   },
+//   get totalPages() {
+//     return this.pages;
+//   },
+//   set totalPages(pgs) {
+//     this.pages = Number(pgs);
+//   },
+//   incrementPage() {
+//     if (this.page === this.pages) {
+//       return;
+//     }
+//     this.page += 1;
+//   },
+//   decrementPage() {
+//     if (this.page === 1) {
+//       return;
+//     }
+//     this.page -= 1;
+//   },
+//   resetPage() {
+//     this.page = 1;
+//   },
+//   currentPage() {
+//     return this.page;
+//   },
+//   fetchMoviesPopularDay,
+//   fetchMoviesWithQuery,
+//   fetchMovieDetails,
+//   fetchMovieCredits,
+//   fetchMovieReviews,
+//   IMG_SERVER,
+// };
 
 export default {
   page: 1,
-  pages: 2,
+  total_pages: null,
   query: '',
+
+  fetchMoviesPopularDay() {
+    return axios
+      .get(`/trending/movie/day?page=${this.page}&api_key=${KEY}`)
+      .then(responce => {
+        return responce.data;
+      });
+  },
+
+  fetchMoviesWithQuery() {
+    return axios
+      .get(`/search/movie?query=${this.query}&api_key=${KEY}&page=${this.page}`)
+      .then(responce => {
+        return responce.data;
+      });
+  },
+
+  fetchMovieDetails(id) {
+    return axios.get(`/movie/${id}?api_key=${KEY}`).then(responce => {
+      return responce.data;
+    });
+  },
+
+  fetchMovieCredits(id) {
+    return axios.get(`/movie/${id}/credits?api_key=${KEY}`).then(responce => {
+      return responce.data.cast;
+    });
+  },
+
+  fetchMovieReviews(id) {
+    return axios.get(`/movie/${id}/reviews?api_key=${KEY}`).then(responce => {
+      return responce.data.results;
+    });
+  },
+
   get searchQuery() {
     return this.query;
   },
@@ -49,13 +126,13 @@ export default {
     this.query = encodeURIComponent(str);
   },
   get totalPages() {
-    return this.pages;
+    return this.total_pages;
   },
   set totalPages(pgs) {
-    this.pages = Number(pgs);
+    this.total_pages = Number(pgs);
   },
   incrementPage() {
-    if (this.page === this.pages) {
+    if (this.page === this.total_pages) {
       return;
     }
     this.page += 1;
@@ -72,10 +149,6 @@ export default {
   currentPage() {
     return this.page;
   },
-  fetchMoviesPopularDay,
-  fetchMoviesWithQuery,
-  fetchMovieDetails,
-  fetchMovieCredits,
-  fetchMovieReviews,
+
   IMG_SERVER,
 };
